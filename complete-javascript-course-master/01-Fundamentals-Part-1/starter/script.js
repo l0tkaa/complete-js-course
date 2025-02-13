f5abf7fe7d3adab5196d4ab982df763c9b021d91
l0tkaa <106283020+l0tkaa@users.noreply.github.com>
2025-02-13 08:18:51 -0600

    initial commit

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,48 @@
+/*let js = 'amazing';
+
+
+console.log(40 + 8+ 23-10);
+
+console.log("jonas");
+
+let firstName = "Jonas";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let country = "USA";
+let continent = "North America"; 
+let  population = 567890;
+console.log(country, continent, population);
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+//birthYear =1990; - does not work
+
+//use const by default and let only when you are sure you will change the variable
+*/
+
+/*
+const age = 19;
+
+if(age>=18){
+    console.log(`Sarah can start driving license`);
+}else{
+    const yearsLeft = 18-age;
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years`);//use backticks- not single quotes
+}
+*/
+
+const favorite = Number(prompt("What's your favorite number"));
+console.log(favorite);
+console.log(typeof favorite);
+
+
+if(favorite === 23){
+    console.log('Cool, great number!');
+}
+
---
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/*let hasDriversLicense = false;
+const passTest = true;
+
+
+if(passTest) hasDriversLicense = true;
+if(hasDriversLicense) console.log('I can drive :D');
+
+//const interface = 'Audio';
+//const private = 534;
+*/
+
+/*function logger(){
+    console.log('My name is jonas');
+}
+
+//calling, running, invoking (same thing)
+
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges){
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+*/
+dot and bracket notation- udemy
+6:39
+
+
